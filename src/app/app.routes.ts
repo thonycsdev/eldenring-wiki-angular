3c56235db66586073a9d9535ddb4cdecac981ce9
thonycsdev <thonycout@gmail.com>
2024-02-24 16:50:10 -0300

    colocando os links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AnatasiaComponentComponent } from './components/anatasia-component/anatasia-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "weapons", component: AnatasiaComponentComponent}
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { User } from '../../../types/user';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  @Input() user: User | null = null
 }
 
