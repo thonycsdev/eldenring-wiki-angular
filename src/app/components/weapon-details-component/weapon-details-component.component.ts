4cbbb51012d31c13ad04c0732c3cd825a586eb87
thonycsdev <thonycout@gmail.com>
2024-02-27 16:42:50 -0300

    Consertando reload perdendo dados da arma

--- src/app/components/weapon-details-component/weapon-details-component.component.ts
+++ src/app/components/weapon-details-component/weapon-details-component.component.ts
@@ -20,11 +20,34 @@ export class WeaponDetailsComponentComponent {
     inject(WeaponsService);
   weaponId: string = "";
   constructor() {
-    this.weaponId =
-      this.router.snapshot.params["id"];
+    this.defineWeaponIdOnReload();
+    this.fetchCurrentWeaponData();
   }
 
-  ngOnInit() {
+
+  private defineWeaponIdOnReload(){
+    const weaponId = this.router.snapshot.params["id"];
+    const storedValue = localStorage.getItem("weaponId");
+    if (weaponId === storedValue) {
+      this.weaponId = weaponId;
+      return;
+    }
+    if(weaponId && weaponId != storedValue){
+      localStorage.setItem("weaponId", weaponId);
+      this.weaponId = weaponId;
+      return;
+    }
+    if(!storedValue){
+      localStorage.setItem("weaponId", weaponId);
+      this.weaponId = weaponId;
+      return;
+    }
+    if(!weaponId){
+      this.weaponId = storedValue;
+      return;
+    }
+  }
+  private fetchCurrentWeaponData(){
     this.service
       .fetchWeaponById(this.weaponId)
       .then((r) => {
